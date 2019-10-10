1945f72e42c4fd158f1dc1e2e34705b25f61f422
BaneshJathar <baneshjathar@gmail.com>
2019-10-10 11:18:14 +0530

    Remove unnecessary parts

--- src/app.js
+++ src/app.js
@@ -1,37 +1,3 @@
-// const path =require('path');
-// const express = require('express');
-
-//  const app = express()
-//  const publicDirectoryPath =  path.join(__dirname,'../public');
-
-//  app.set('view engine', 'hbs')
-// app.use(express.static(publicDirectoryPath))
-
-// app.get('', (req,res) => {
-//     res.render('index')
-// })
-
-// app.get('/weather', (req,res) => {
-//        res.send("Weather page");
-//    })
-
-// app.listen(3000, () => {
-//        console.log('Server is up on port  3000.');
-//    });
-
-
-
-
-
-
-
-
-
-
-
-//*************************       Sir Code  *********** */
-
-
 
 const path = require('path')
 const express = require('express')
